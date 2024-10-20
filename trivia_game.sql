e1753081c274f6436d43b3aab8e7f556acecac01
amirkle27 <amirkle@gmail.com>
2024-10-20 23:29:21 +0300

    Update trivia_game.sql

--- trivia_game.sql
+++ trivia_game.sql
@@ -2,37 +2,42 @@ select count (*) from questions;
  select * from questions;
 
 
-drop function new_player;
+DROP FUNCTION IF EXISTS new_player;
 
 CREATE OR REPLACE FUNCTION new_player(
-        new_username VARCHAR(50),
-        new_password TEXT,
-        new_email VARCHAR(100),
-        new_age INTEGER,
-        OUT player_id INTEGER
-        )
-    language plpgsql AS
-    $$
-    begin
-        IF EXISTS(SELECT 1 FROM players WHERE players.username = new_username ) THEN
-            RAISE EXCEPTION 'This username already exists. Try something else';
-        END IF;
-        IF EXISTS(SELECT 1 FROM players WHERE players.password = new_password) THEN
-            RAISE EXCEPTION 'This password already exists. Try something else';
-        END IF;
-        IF EXISTS(SELECT 1 FROM players WHERE players.email = new_email) THEN
-            RAISE EXCEPTION 'This email already exists. Try something else';
-        ELSE
-            INSERT INTO players (username, password, email, age) VALUES (
-                new_username,
-                new_password,
-                new_email,
-                new_age)
-            RETURNING player_id into player_id;
-        END IF;
+    new_username VARCHAR(50),
+    new_password TEXT,
+    new_email VARCHAR(100),
+    new_age INTEGER,
+    OUT out_player_id INTEGER
+)
+LANGUAGE plpgsql AS
+$$
+BEGIN
+    -- Check for existing username
+    IF EXISTS(SELECT 1 FROM players WHERE players.username = new_username) THEN
+        RAISE EXCEPTION 'This username already exists. Try something else';
+    END IF;
 
-    end;
+    -- Check for existing password
+    IF EXISTS(SELECT 1 FROM players WHERE players.password = new_password) THEN
+        RAISE EXCEPTION 'This password already exists. Try something else';
+    END IF;
+
+    -- Check for existing email
+    IF EXISTS(SELECT 1 FROM players WHERE players.email = new_email) THEN
+        RAISE EXCEPTION 'This email already exists. Try something else';
+    ELSE
+        -- Insert into players and return player_id
+        INSERT INTO players (username, password, email, age)
+        VALUES (new_username, new_password, new_email, new_age)
+        RETURNING player_id INTO out_player_id;
+    END IF;
+END;
 $$;
+
+
+
 
 
 
@@ -53,10 +58,14 @@ AFTER INSERT ON players
 FOR EACH ROW
 EXECUTE FUNCTION log_new_player();
 
-ALTER TABLE players DISABLE TRIGGER new_user_registration;
+
 SELECT setval('players_player_id_seq', (SELECT MAX(player_id) FROM players));
 DELETE FROM players;
 select * from players;
 select * from player_answers;
 select * from new_player_log;
 ALTER SEQUENCE players_player_id_seq RESTART WITH 1;
+
+-- drop procedure add_answer_to_player_answers
+--
+-- CREATE OR REPLACE PROCEDURE add_answer_to_player_answers()
