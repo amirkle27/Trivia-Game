243e776e933d0dbc1d3eea3c418d35792d361052
amirkle27 <amirkle@gmail.com>
2024-10-25 16:23:33 +0300

    Update trivia_game.sql

--- trivia_game.sql
+++ trivia_game.sql
@@ -47,10 +47,34 @@ CREATE OR REPLACE FUNCTION log_new_player()
     $$ LANGUAGE plpgsql;
 
 drop TRIGGER IF EXISTS new_user_registration ON players;
+
 CREATE TRIGGER new_user_registration
 AFTER INSERT ON players
 FOR EACH ROW
 EXECUTE FUNCTION log_new_player();
+
+drop function get_random_questions;
+
+CREATE OR REPLACE FUNCTION get_random_questions ()
+    RETURNS TABLE (
+        question_id INTEGER,
+        question_text TEXT,
+        answer_a TEXT,
+        answer_b TEXT,
+        answer_c TEXT,
+        answer_d TEXT,
+        correct_answer CHAR(1))
+language plpgsql AS
+    $$
+    begin
+        RETURN QUERY
+        SELECT q.question_id, q.question_text, q.answer_a, q.answer_b, q.answer_c, q.answer_d, q.correct_answer
+        FROM questions q
+        ORDER BY RANDOM()
+        LIMIT 20;
+        END
+    $$;
+
 
 
 drop procedure update_player_answers;
@@ -74,10 +98,7 @@ language plpgsql AS
             WHERE q.question_id = p_question_id;
 
             INSERT INTO player_answers(player_id, question_id, selected_answer, is_correct)
-            VALUES (p_player_id,p_question_id,p_selected_answer,v_is_correct)
-            ON CONFLICT (player_id,question_id)
-            DO UPDATE
-            SET selected_answer = EXCLUDED.is_correct;
+            VALUES (p_player_id,p_question_id,p_selected_answer,v_is_correct);
 
             UPDATE players
             SET questions_solved=questions_solved+1
@@ -87,36 +108,84 @@ language plpgsql AS
 
 $$;
 
-drop procedure update_high_score_table;
+drop procedure update_high_score_table_start_time;
 
-CREATE OR REPLACE procedure update_high_score_table(
+CREATE OR REPLACE procedure update_high_score_table_start_time(
+    IN p_player_id INTEGER)
+language plpgsql AS
+    $$
+        begin
+            INSERT INTO high_scores (player_id, started_at)
+            VALUES (p_player_id, CURRENT_TIMESTAMP);
+        end;
+    $$;
+
+
+drop procedure update_high_score_table_finish_time;
+
+CREATE OR REPLACE procedure update_high_score_table_finish_time(
     IN p_player_id INTEGER)
 language plpgsql AS
     $$
     DECLARE v_correct_answer INTEGER;
+            v_latest_score_id INTEGER;
         begin
             SELECT COUNT (*)
             INTO v_correct_answer
             FROM player_answers
             WHERE player_id = p_player_id AND is_correct = TRUE;
 
-            IF v_correct_answer > 5 THEN
-                INSERT INTO high_scores (player_id, score_id, finished_at)
-                VALUES (p_player_id, v_correct_answer, CURRENT_TIMESTAMP);
+            IF v_correct_answer >= 5 THEN
+                SELECT score_id
+                INTO v_latest_score_id
+                FROM high_scores
+                WHERE player_id = p_player_id
+                ORDER BY started_at DESC
+                LIMIT 1;
+
+                UPDATE high_scores
+                SET finished_at = CURRENT_TIMESTAMP,
+                    total_game_time = (CURRENT_TIMESTAMP - started_at),
+                    score = v_correct_answer
+                WHERE  score_id = v_latest_score_id;
             END IF;
         end;
     $$;
 
+drop function maintain_top_20_scores;
 
+CREATE OR REPLACE FUNCTION maintain_top_20_scores()
+RETURNS TRIGGER
+language plpgsql AS
+    $$
+    begin
+        DELETE FROM high_scores
+        WHERE score_id NOT IN
+              (SELECT score_id
+                FROM high_scores
+                ORDER BY score DESC
+                LIMIT 20);
+        RETURN NEW;
+    end;
+    $$;
 
+drop TRIGGER limit_high_scores ON high_scores;
+
+CREATE TRIGGER limit_high_scores
+AFTER INSERT ON high_scores
+FOR EACH ROW
+EXECUTE FUNCTION maintain_top_20_scores();
 
 SELECT setval('players_player_id_seq', (SELECT MAX(player_id) FROM players));
 DELETE FROM players;
 select * from players;
 select * from player_answers;
 select * from new_player_log;
+select * from high_scores;
+select * from questions;
 ALTER SEQUENCE players_player_id_seq RESTART WITH 1;
 
 -- drop procedure add_answer_to_player_answers
---
+--f
 -- CREATE OR REPLACE PROCEDURE add_answer_to_player_answers()
+
